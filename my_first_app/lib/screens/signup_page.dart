2d860797159bf3dc5905ca9535b17ad995b1b666
Muhammad Mahad Siddiqui <m.mahadsiddiquii@gmail.com>
2026-02-16 19:13:10 +0500

    Enhance SignUpPage with improved UI elements and additional input fields

--- my_first_app/lib/screens/signup_page.dart
+++ my_first_app/lib/screens/signup_page.dart
@@ -13,7 +13,7 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,     
+      backgroundColor: const Color(0xFF1E3A5F),
       body:SafeArea(
         child:Center(
           child:SingleChildScrollView(
@@ -31,7 +31,7 @@ class _SignUpPageState extends State<SignUpPage> {
                     color: Colors.white,
                   ),
                 ),
-                const SizedBox(height: 40),
+                const SizedBox(height: 100),
                 // form
                   TextField(
                   decoration: InputDecoration(
@@ -58,6 +58,149 @@ class _SignUpPageState extends State<SignUpPage> {
                   ),
                   style: const TextStyle(color: Colors.white),
                 ),
+                 const SizedBox(height: 16),
+                 TextField(
+                  obscureText: _obscurePassword,
+                  decoration: InputDecoration(
+                    hintText: '••••••••',
+                    hintStyle: const TextStyle(
+                      color: Color(0xFF5A6B7E),
+                      fontSize: 14,
+                    ),
+                    labelText: 'Password',
+                    labelStyle: const TextStyle(
+                      color: Color.fromARGB(255, 146, 155, 119),
+                      fontSize: 12,
+                    ),
+                    fillColor: const Color.fromARGB(255, 198, 202, 203),
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 16,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscurePassword
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                        color: const Color.fromARGB(255, 235, 242, 251),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'mahad',
+                    hintStyle: const TextStyle(
+                      color: Color(0xFF5A6B7E),
+                      fontSize: 14,
+                    ),
+                    labelText: 'First Name',
+                    labelStyle: const TextStyle(
+                      color: Color.fromARGB(255, 146, 155, 119),
+                      fontSize: 12,
+                    ),
+                    fillColor: const Color.fromARGB(255, 198, 202, 203),
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 16,
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Siddiqui',
+                    hintStyle: const TextStyle(
+                      color: Color(0xFF5A6B7E),
+                      fontSize: 14,
+                    ),
+                    labelText: 'Last Name',
+                    labelStyle: const TextStyle(
+                      color: Color.fromARGB(255, 146, 155, 119),
+                      fontSize: 12,
+                    ),
+                    fillColor: const Color.fromARGB(255, 198, 202, 203),
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 16,
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+                  const SizedBox(height: 80),
+                  
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFFFC107),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      elevation: 0,
+                    ),
+                    child: const Text(
+                      'Log In',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have account? ",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 14,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Color(0xFFFFA726),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                
+
+                
 
               ],
             )
